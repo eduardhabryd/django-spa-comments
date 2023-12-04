704e8324338b0b341de935adae15a397d6a83501
eduardhabryd <eduardhabryd@gmail.com>
2023-12-04 22:32:57 +0200

    implement comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    home_page = models.URLField(blank=True)
+    text = models.TextField()
+    parent_comment = models.ManyToManyField(
+        'self',
+        blank=True,
+        symmetrical=False,
+        related_name="replies"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user_name} - {self.created_at}'
