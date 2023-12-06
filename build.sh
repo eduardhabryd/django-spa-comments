895bcf3277966cf1231e8f52ba73c5ec70856ef8
eduardhabryd <eduardhabryd@gmail.com>
2023-12-06 21:00:21 +0200

    create script for deployment

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py loaddata fixture.json
